fa34fd99fe5b3a6eb50602f75fee4b8af8176785
checo612 <40772974+checo612@users.noreply.github.com>
2018-09-25 14:47:22 -0500

    first commit

---
+++ TFAndroid/MainActivity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.TextToSpeech;
+
+namespace TFAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public Android.Support.V7.Widget.Toolbar Toolbar { get; set; }
+
+        Button takePhotoButton;
+        private Button TakePhotoButton => takePhotoButton ?? (takePhotoButton = FindViewById<Button>(Resource.Id.take_photo_button));
+
+        ImageView photoView;
+        private ImageView PhotoView => photoView ?? (photoView = FindViewById<ImageView>(Resource.Id.photo));
+
+        TextView resultLabel;
+        private TextView ResultLabel => resultLabel ?? (resultLabel = FindViewById<TextView>(Resource.Id.result_label));
+
+        ProgressBar progressBar;
+        private ProgressBar ProgressBar => progressBar ?? (progressBar = FindViewById<ProgressBar>(Resource.Id.progressbar));
+
+        readonly ImageClassifier imageClassifier = new ImageClassifier();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            Toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            if (Toolbar != null)
+                SetSupportActionBar(Toolbar);
+
+            SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+            SupportActionBar.SetHomeButtonEnabled(false);
+
+            TakePhotoButton.Click += TakePhotoButton_Click;
+        }
+
+        async void TakePhotoButton_Click(object sender, EventArgs e)
+        {
+            TakePhotoButton.Enabled = false;
+            ProgressBar.Visibility = ViewStates.Visible;
+
+            try
+            {
+                var image = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions { PhotoSize = PhotoSize.Medium });
+                var bitmap = await BitmapFactory.DecodeStreamAsync(image.GetStreamWithImageRotatedForExternalStorage());
+
+                PhotoView.SetImageBitmap(bitmap);
+                var result = await Task.Run(() => imageClassifier.RecognizeImage(bitmap));
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                CrossTextToSpeech.Current.Speak($"El objeto es {result}");
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                ResultLabel.Text = result;
+            }
+            finally
+            {
+                TakePhotoButton.Enabled = true;
+                ProgressBar.Visibility = ViewStates.Invisible;
+            }
+        }
+
+    }
+}
+
---
+++ TFAndroid/obj/Debug/MonoAndroid81/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("TFAndroid.MainApplication, TFAndroid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md58d0228dd8ca48aa83133a622f74e2e5f.MainApplication.class, md58d0228dd8ca48aa83133a622f74e2e5f.MainApplication.__md_methods);
+		
+	}
+}
